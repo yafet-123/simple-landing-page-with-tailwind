ef914674d33d963c6443accfbe9d886a16275294
yafet-123 <yafetaddisu123@gmail.com>
2022-08-29 14:28:35 +0300

    first commit

--- components/Testimonials.js
+++ components/Testimonials.js
@@ -4,7 +4,7 @@ export default function Testimonials(){
         <section id="testimonials">
             <div className="max-w-6xl px-5 mx-auto mt-32 text-center">
                 <h2 className="text-4xl font-bold text-center">
-                    What's Different About Manage?
+                    What is Different About Manage?
                 </h2>
                 <div className="my-5 flex flex-col mt-24 md:flex-row md:space-x-6">
                     <div className="flex flex-col items-center p-6 space-y-6 rounded-lg bg-slate-100 md:w-1/3">
